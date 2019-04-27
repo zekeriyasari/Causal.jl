c59e79a7818a5ebb3073d0a767cbe908b671080f
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2019-04-28 01:16:36 +0300

    Completed JuSDL plugins and adding user defined plugins

--- src/plugins/Plugins.jl
+++ src/plugins/Plugins.jl
@@ -2,6 +2,9 @@
 
 @reexport module Plugins
 
+using LibGit2
+using Random
+
 abstract type AbstractPlugin end
 
 # Define generic plugin functions.
@@ -12,19 +15,52 @@ function check end
 function add end
 function remove end
 
-# Includes all the plugin files
-for file in readdir(@__DIR__)
-    (startswith(file, "Plugins.jl") || startswith(file, ".")) || include(file)
+function search(rootpath::String, filename)
+    paths = String[]
+    for (root, dirs, files) in walkdir(rootpath)
+        for file in files
+            if occursin(filename, file)
+                push!(paths, joinpath(root, file))
+            end
+        end
+    end
+    paths
 end
 
-# Include user defines plugins if exists
-pluginpath = joinpath(pwd(), "plugins")
-if ispath(pluginpath)
-    for file in readdir(pluginpath)
-        filepath = joinpath(pluginpath, file)
-        startswith(file, ".") || include(filepath)
-        @info "Included plugin file" filepath
+const remote_repo_url = "https://imel.eee.deu.edu.tr/git/JuSDLPlugins.jl.git"
+
+function add(name, url=remote_repo_url)
+    startswith(name, ".") && error("Name of plugin should not start with `.`")
+    startswith(name, "Plugins") && error("Name of plugin cannot be `Plugins`")
+    startswith(".jl", name) || (name *= ".jl")
+    
+    repopath = joinpath("/tmp", "JuSDLPlugins", randstring())
+    ispath(repopath) || mkpath(repopath)
+    @info "Cloning avaliable plugins from $url"
+    LibGit2.clone(url, repopath)
+    @info "Done..."
+
+    @info "Searching for $name in plugins repo."
+    srcpath = search(joinpath(repopath, "src"), name)[1]
+    if isempty(srcpath)
+        error("$name could not be found in avaliable plugins")
+    else
+        dstdir = joinpath(@__DIR__, "additionals")
+        dstpath = joinpath(dstdir, name)
+        cp(srcpath, dstpath, force=true)
+        include(dstpath)
+        @info "$name is added to JuSDL.Plugins"
     end
 end
+
+# Includes essential plugins from JuSDL
+foreach(include, search(joinpath(@__DIR__, "essentials"), ".jl"))
+
+# Includes additional plugins from JuSDL
+foreach(include, search(joinpath(@__DIR__, "additionals"), ".jl"))
+
+# Include plugins from user working directory.
+user_plugins_path = joinpath(pwd(), "plugins")
+ispath(user_plugins_path) && foreach(include, search(joinpath(user_plugins_path), ".jl"))
 
 end  # module
---
+++ src/plugins/additionals/template.jl
@@ -0,0 +1,5 @@
+# This file includes the template plugin
+
+struct TemplatePlugin
+end
+process(plg::TemplatePlugin, x) = println("In the template plugin. Doing nothing")

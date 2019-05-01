61927fdf9ccf4017c06390698a34f98b1265683a
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2019-05-01 03:05:19 +0300

    Revised examples

--- example/driven_noisy_chua.jl
+++ example/driven_noisy_chua.jl
@@ -3,14 +3,15 @@
 using JuSDL
 import JuSDL.Plugins.Lyapunov
 using Plots
+using LaTeXStrings
 
 # Construct clock.
-clk = Clock(0., 0.01, 100.)
+clk = Clock(0., 0.01, 250.)
 
 # Construct ode system 
-q(x, a=-8/7, b=-5/7) = b*x + 1 / 2 * (a - b) * (abs(x + 1) - abs(x - 1)) 
-function f1(dx, x, u, t, alpha=15.6, beta=28, q=q)
-    dx[1] = alpha * (x[2] - x[1] - q(x[1]))
+gamma(x, a=-8/7, b=-5/7) = b*x + 1 / 2 * (a - b) * (abs(x + 1) - abs(x - 1)) 
+function f1(dx, x, u, t, alpha=15.6, beta=28, gamma=gamma)
+    dx[1] = alpha * (x[2] - x[1] - gamma(x[1]))
     dx[2] = x[1] - x[2] + x[3]
     dx[3] = -beta * x[2]
 end
@@ -20,12 +21,12 @@ t = 0.
 odeds = ODESystem(f1, g1, x0, t)
 
 # Construct sde system 
-function f2(dx, x, u, t, alpha=9, beta=100/7, q=q)
-    dx[1] = alpha * (x[2] - x[1] - q(x[1])) + u[1]
+function f2(dx, x, u, t, alpha=15.6, beta=28, gamma=gamma)
+    dx[1] = alpha * (x[2] - x[1] - gamma(x[1])) + u[1]
     dx[2] = x[1] - x[2] + x[3]
     dx[3] = -beta * x[2]
 end
-function h2(dx, x, u, t, eta=0.05)
+function h2(dx, x, u, t, eta=0.1)
     dx[1] = -eta
     dx[2] = eta
     dx[3] = 0
@@ -39,35 +40,39 @@ sdeds = SDESystem((f2, h2), g2, x0, t, Bus(3))
 gain = Gain([0.01, 0., 0.])
 
 # Construct sinks.
-odewriter = Writer(Bus(3), buflen=5000, plugin=nothing)
-odeprinter = Printer(Bus(1), buflen=5000, plugin=Lyapunov(ts=clk.dt, m=7, J=11, ni=200))
-sdewriter = Writer(Bus(3), buflen=5000, plugin=nothing)
+writer1 = Writer(Bus(3), buflen=5000, plugin=nothing)
+writer2 = Writer(Bus(3), buflen=5000, plugin=nothing)
+writer3 = Writer(Bus(1), buflen=5000, plugin=Lyapunov(ts=clk.dt, m=7, J=11, ni=200))
 
 # Connect the components
 connect(odeds.output, gain.input)
 connect(gain.output, sdeds.input)
-connect(odeds.output, odewriter.input)
-connect(odeds.output[1], odeprinter.input)
-connect(sdeds.output, sdewriter.input)
+connect(odeds.output, writer1.input)
+connect(odeds.output[1], writer3.input)
+connect(sdeds.output, writer2.input)
 
 # Construct the model 
-model = Model(odeds, sdeds, gain, odewriter, odeprinter, sdewriter, clk=clk)
+model = Model(odeds, sdeds, gain, writer1, writer3, writer2, clk=clk)
 
 # Simulate the model 
 sim = simulate(model);
 
 # Read back the simulation data.
-odecontent = read(odewriter)
-sdecontent = read(sdewriter)
+writer1content = read(writer1)
+writer2content = read(writer2)
+writer3content = read(writer3)
 
 # PLot the simulation data.
-plt1 = plot()
-for (i, k) in enumerate(keys(odecontent))
-    plot!(odecontent[k][:, 1], odecontent[k][:, 2], label=string(i))
+plt1 = plot(xlabel=L"$x$", ylabel=L"$y$", size=(500, 200))
+for (i, t) in enumerate(keys(writer1content))
+    plot!(writer1content[t][:, 1], writer1content[t][:, 2], label=string(i), lw=1.5)
 end
 plt1
-plt2 = plot()
-for (i, k) in enumerate(keys(sdecontent))
-    plot!(sdecontent[k][:, 1], sdecontent[k][:, 2], label=string(i))
+plt2 = plot(xlabel=L"$x$", ylabel=L"$y$", size=(500, 200))
+for (i, t) in enumerate(keys(writer2content))
+    plot!(writer2content[t][:, 1], writer2content[t][:, 2], label=string(i), lw=1.5)
 end
 plt2
+for (i, t) in enumerate(keys(writer3content))
+    println(writer3content[t])
+end
--- example/noisy_chua_system.jl
+++ example/noisy_chua_system.jl
@@ -3,25 +3,26 @@
 using JuSDL
 import JuSDL.Plugins.Fft
 using Plots
+using LaTeXStrings
 
 # Construct the components
-q(x, a=-1.143, b=-0.714) = b*x + 1 / 2 * (a - b) * (abs(x + 1) - abs(x - 1)) 
-function f(dx, x, u, t, alpha=15.6, beta=28, q=q)
-    dx[1] = alpha * (x[2] - x[1] - q(x[1]))
+gamma(x, a=-1.143, b=-0.714) = b*x + 1 / 2 * (a - b) * (abs(x + 1) - abs(x - 1)) 
+function f(dx, x, u, t, alpha=15.6, beta=28, gamma=gamma)
+    dx[1] = alpha * (x[2] - x[1] - gamma(x[1]))
     dx[2] = x[1] - x[2] + x[3]
     dx[3] = -beta * x[2]
 end
-function h(dx, x, u, t, eta=0.05)
+function h(dx, x, u, t, eta=0.1)
     dx[1] = -eta
     dx[2] = eta
     dx[3] = 0
 end
 g(x, u, t) = [x[1], x[2], x[3]]
-x0 = [1e-6, 1e-6, 1e-6]
+x0 = rand(3)*1e-3
 t = 0.
 sdeds = SDESystem((f, h), g, x0, t)
-writer = Writer(Bus(3), buflen=5000, plugin=nothing)
-clk = Clock(0., 0.005, 100.)
+writer = Writer(Bus(3), buflen=2000, plugin=nothing)
+clk = Clock(0., 0.01, 100.)
 
 # Connect the components
 connect(sdeds.output, writer.input)
@@ -36,11 +37,11 @@ sim = simulate(model)
 content = read(writer)
 
 # PLot the simulation data.
-plt1 = plot()
-for (i, k) in enumerate(keys(content))
-    plot!(k, content[k][:, 1], label=string(i))
+plt1 = plot(xlabel=L"$t$", ylabel=L"$x$", size=(500, 200), legend=:right)
+for (i, t) in enumerate(keys(content))
+    plot!(t, content[t][:, 1], label=string(i), lw=1.5)
 end
-plt2 = plot()
-for (i, k) in enumerate(keys(content))
-    plot!(content[k][:, 1], content[k][:, 2], label=string(i))
+plt2 = plot(xlabel=L"$x$", ylabel=L"$y$", size=(500, 200))
+for (i, t) in enumerate(keys(content))
+    plot!(content[t][:, 1], content[t][:, 2], label=string(i), lw=1.5)
 end

d5d74cb084ca03d60bf651e4dfef81bd25d17243
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2019-10-12 21:58:20 +0300

    Changed dynamic system constructor argument order.

---
+++ example/workspace.jl
@@ -0,0 +1,27 @@
+using Jusdl 
+using Plots 
+
+# Define the network parameters 
+numnodes = 2
+nodes = [LorenzSystem(Bus(3), Bus(3)) for i = 1 : numnodes]
+adjmat = [-10 10; 10 -10]
+cplmat = [1 0 0; 0 0 0; 0 0 0]
+net = Network(nodes, adjmat, cplmat)
+writer = Writer(Bus(length(net.output)))
+
+# Connect the blocks
+connect(net.output, writer.input)
+
+# Construct the model 
+model = Model(net, writer)
+
+# Simulate the model 
+sim = simulate(model, 0, 0.01, 10)
+
+# Read and process the simulation data.
+content = read(writer)
+t = vcat(collect(keys(content))...)
+x = collect(hcat(vcat(collect(values(content))...)...)')
+plot(t, x[:, 1])
+plot!(t, x[:, 4])
+plot(t, abs.(x[:, 1] - x[:, 4]))

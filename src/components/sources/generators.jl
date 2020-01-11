eaa1b986140b726d2279dc832c0ac6c6e093f67d
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-01-11 16:10:35 +0300

    Some syntax correction in docs .

--- src/components/sources/generators.jl
+++ src/components/sources/generators.jl
@@ -28,7 +28,7 @@ end
 
 Constructs a `SinewaveGenerator` with output of the form
 ```math 
-    x(t) = A sin(2 * \pi f * (t - \tau) + \phi) + B
+    x(t) = A sin(2 \pi f  (t - \tau) + \phi) + B
 ```
 where ``A`` is `amplitude`, ``f`` is `frequency`, ``\tau`` is `delay` and ``\phi`` is `phase` and ``B`` is `offset`.
 """

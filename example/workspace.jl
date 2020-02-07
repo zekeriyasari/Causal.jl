771998090957c3d9a23c123c34810f0bcd0eb993
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-02-07 11:43:05 +0300

    Added testset for Network.

--- example/workspace.jl
+++ example/workspace.jl
@@ -1,27 +1,4 @@
-using Jusdl 
 
-clk = Clock(0., 1., 10.)
-set!(clk)
-@show [take!(clk) for i = 0 : 10]
-
-clk = Clock(0., 1., 10.)
-set!(clk)
-for t in clk 
-    @show t
-end
-
-clk = Clock(0., 1., 10.)
-set!(clk)
-vals = []
-for t in clk 
-    @show t
-    push!(vals, t)
-end
-@show vals
-
-chnl = Channel() do ch
-    foreach(i -> put!(ch, i), 1:4)
-end;
-
-vals = [take!(chnl) for i = 1 : 4]
-@show vals
+main(name::Symbol, args...; kwargs...) = eval(name)(args...; kwargs...)
+f(x, y) = x + y
+g(x, y; z=4) = x + y + z
---
+++ test/test_components/test_systems/test_network.jl
@@ -0,0 +1,94 @@
+# This file includes testset for Network
+
+@testset "NetworkTestSet" begin 
+    # Network construction 
+    nodenum =  5
+    nodedim = 3
+    nodes = [LorenzSystem(Bus(nodedim), Bus(nodedim)) for i = 1 : nodenum]
+    conmat = [-1 1; 1 -1]
+    cplmat = [1 0 0; 0 0 0; 0 0 0]
+    net = Network(nodes, conmat, cplmat)
+    @test typeof(net.trigger) == Link{Float64}
+    @test typeof(net.handshake) == Link{Bool}
+    @test net.input == nothing
+    @test typeof(net.output) == Bus{Link{Float64}}
+    @test length(net.output) == nodenum * nodedim
+    @test length(filter(comp -> isa(comp, Memory), net.components)) == nodenum
+    @test length(filter(comp -> isa(comp, Coupler), net.components)) == 1
+    @test net.clusters == [1 : nodenum]
+
+    # More on Network construction 
+    conmat = (a = ones(nodenum, nodenum); map(i -> a[i, i] = -(nodenum - 1), 1 : nodenum); a)
+    net = Network(nodes, conmat, cplmat; inputnodeidx=[], outputnodeidx=[])
+    @test net.input === nothing
+    @test net.output === nothing
+    net = Network(nodes, conmat, cplmat; inputnodeidx=[], outputnodeidx=[1, 3]) 
+    @test net.input === nothing
+    @test typeof(net.output) == Bus{Link{Float64}}
+    @test length(net.output) == 2 * nodedim
+    net = Network(nodes, conmat, cplmat; inputnodeidx=[], outputnodeidx=collect(1:nodenum), clusters=[1:2, 3:5]) 
+    @test net.clusters == [1:2, 3:5]
+
+    # Network topologies
+    @test coupling(3, 1) == [1 0 0; 0 0 0; 0 0 0]
+    @test coupling(3, [1,2]) == [1 0 0; 0 1 0; 0 0 0]
+    @test coupling(3, [1,3]) == [1 0 0; 0 0 0; 0 0 1]
+
+    @test topology(:path_graph, 5) == [-1.0   1.0  -0.0  -0.0  -0.0;
+                                        1.0  -2.0   1.0  -0.0  -0.0;
+                                        -0.0   1.0  -2.0   1.0  -0.0;
+                                        -0.0  -0.0   1.0  -2.0   1.0;
+                                        -0.0  -0.0  -0.0   1.0  -1.0]
+    @test topology(:path_graph, 5, weight=10) == [-1.0   1.0  -0.0  -0.0  -0.0;
+                                                1.0  -2.0   1.0  -0.0  -0.0;
+                                                -0.0   1.0  -2.0   1.0  -0.0;
+                                                -0.0  -0.0   1.0  -2.0   1.0;
+                                                -0.0  -0.0  -0.0   1.0  -1.0] * 10
+    conmat = topology(:path_graph, 5, weight=10, timevarying=true)
+    @test size(conmat) == (5, 5)
+    @test isa(conmat, Matrix{<:Function})
+    @test map(f -> f(1.), conmat) == [-1.0   1.0  -0.0  -0.0  -0.0;
+                                        1.0  -2.0   1.0  -0.0  -0.0;
+                                        -0.0   1.0  -2.0   1.0  -0.0;
+                                        -0.0  -0.0   1.0  -2.0   1.0;
+                                        -0.0  -0.0  -0.0   1.0  -1.0] * 10
+    @test topology(:star_graph, 5) == [-4.0   1.0   1.0   1.0   1.0;
+                                        1.0  -1.0  -0.0  -0.0  -0.0;
+                                        1.0  -0.0  -1.0  -0.0  -0.0;
+                                        1.0  -0.0  -0.0  -1.0  -0.0;
+                                        1.0  -0.0  -0.0  -0.0  -1.0]
+    @test topology(:cycle_graph, 5) == [-2.0   1.0  -0.0  -0.0   1.0;
+                                        1.0  -2.0   1.0  -0.0  -0.0;
+                                        -0.0   1.0  -2.0   1.0  -0.0;
+                                        -0.0  -0.0   1.0  -2.0   1.0;
+                                        1.0  -0.0  -0.0   1.0  -2.0]
+    @test clusterconnectivity(1:2, 3:6) == [-3.0   3.0  -1.0   1.0   0.0   0.0;
+                                            3.0  -3.0   1.0  -1.0   0.0   0.0;
+                                            -1.0   1.0  -9.0   3.0   3.0   3.0;
+                                            1.0  -1.0   3.0  -9.0   3.0   3.0;
+                                            0.0   0.0   3.0   3.0  -9.0   3.0;
+                                            0.0   0.0   3.0   3.0   3.0  -9.0]
+    @test isapprox(cgsconnectivity(:path_graph, 5), [-0.8   0.8   0.0   0.0   0.0;
+                                                0.8  -2.0   1.2   0.0   0.0;
+                                                0.0   1.2  -2.4   1.2   0.0;
+                                                0.0   0.0   1.2  -2.0   0.8;
+                                                0.0   0.0   0.0   0.8  -0.8])
+
+    # Driving Network 
+    nodenum = 2
+    nodedim = 3
+    nodes = [LorenzSystem(Bus(nodedim), Bus(nodedim)) for i = 1 : nodenum]
+    conmat = [-1 1; 1 -1]
+    cplmat = [1 0 0; 0 0 0; 0 0 0]
+    net = Network(nodes, conmat, cplmat)
+    tsk = ComponentTask.(launch(net))
+    for t in 0. : 10.
+        drive(net, t)
+        approve(net)
+    end
+    release(net)
+    terminate(net)
+    sleep(0.1)
+    @test all(istaskdone.(tsk))
+
+end  # testset 

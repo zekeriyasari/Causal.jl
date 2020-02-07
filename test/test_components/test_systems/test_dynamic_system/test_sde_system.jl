4b96251af792f33a7bd26e24279e3ed4da438d8a
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-02-07 20:35:18 +0300

    Added testset for SDESystem.

--- test/test_components/test_systems/test_dynamic_system/test_sde_system.jl
+++ test/test_components/test_systems/test_dynamic_system/test_sde_system.jl
@@ -38,6 +38,17 @@
         dx[3] = -x[3] + cos(u[2](t))
     end
     g2(x, u, t) = x 
-    ds = SDESystem(Bus(3), Bus(2), (f,h), g, ones(3), 0)
+    ds = SDESystem(Bus(2), Bus(3), (f2, h2), g2, ones(3), 0)
+    tsk = launch(ds)
+    for t in 1 : 10 
+        drive(ds, t)
+        put!(ds.input, [t, 2t])
+        approve(ds)
+        @test ds.t == t
+        @test [read(link.buffer) for link in ds.output] == ds.state
+    end
+    terminate(ds)
+    sleep(0.1)
+    @test all(istaskdone.(tsk))
 
 end  # testset

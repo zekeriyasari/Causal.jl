3d4fedca732db657423c0140c80f49e4c4528b8c
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-02-07 22:56:20 +0300

    Added testset for Plugins.

---
+++ test/test_plugins/test_plugins.jl
@@ -0,0 +1,16 @@
+# This file includes testset for Plugin
+
+import Jusdl.Plugins: Fft, Variance, Std, Mean, Lyapunov
+
+@testset "PluginTestSet" begin 
+    plgs = [Fft(), Variance(), Std(), Mean(), Lyapunov()]
+    foreach(plg -> @test(plg.dims == 1), plgs[1 : end - 1])
+    plg = plgs[end]
+    @test plg.m == 15
+    @test plg.J == 5
+    @test plg.ni == 300
+    @test plg.ts == 0.01
+
+    foreach(plg -> @test(applicable(process, plg, rand(100))), plgs)
+end # testset
+

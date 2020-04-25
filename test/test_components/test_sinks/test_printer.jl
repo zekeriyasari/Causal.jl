2fd2dd0616976bc62dfb4e31e6320d4bc79d4d74
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-04-26 02:22:06 +0300

    Revised sink testset.

--- test/test_components/test_sinks/test_printer.jl
+++ test/test_components/test_sinks/test_printer.jl
@@ -1,26 +1,26 @@
-# # This file constains testset for Printer 
+# This file constains testset for Printer 
 
-# @testset "PrinterTestSet" begin 
-#     # Printer construction 
-#     printer = Printer(Bus(2), buflen=100)
-#     @test typeof(printer.trigger) == Link{Float64}
-#     @test typeof(printer.handshake) == Link{Bool}
-#     @test size(printer.timebuf) == (100,)
-#     @test size(printer.databuf) == (2, 100)
-#     @test isa(printer.input, Bus)
-#     @test printer.plugin === nothing
-#     @test !isempty(printer.callbacks)
+@testset "PrinterTestSet" begin 
+    # Printer construction 
+    printer = Printer(Inport(2), buflen=100)
+    @test typeof(printer.trigger) == Inpin{Float64}
+    @test typeof(printer.handshake) == Outpin{Bool}
+    @test size(printer.timebuf) == (100,)
+    @test size(printer.databuf) == (2, 100)
+    @test isa(printer.input, Inport)
+    @test printer.plugin === nothing
+    @test typeof(printer.callbacks) <: Callback
 
-#     # Driving Printer 
-#     tsk = launch(printer)
-#     for t in 1 : 200
-#         drive(printer, t)
-#         put!(printer.input, ones(2) * t)
-#         approve(printer)
-#         @test read(printer.timebuf) == t
-#         @test [read(link.buffer) for link in printer.input] == ones(2) * t
-#     end 
-#     terminate(printer)
-#     sleep(0.1)
-#     @test all(istaskdone.(tsk))
-# end  # testset 
+    # Driving Printer 
+    oport, iport, trg, hnd, tsk, tsk2 = prepare(printer)
+    for t in 1 : 200
+        put!(trg, t)
+        put!(oport, ones(2) * t)
+        take!(hnd)
+        @test read(printer.timebuf) == t
+        @test [read(pin.links[1].buffer) for pin in oport] == ones(2) * t
+    end 
+    put!(trg, NaN)
+    sleep(0.1)
+    @test istaskdone(tsk)
+end  # testset 
